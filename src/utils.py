6acef6a4a4dd4fe29a8678205c35a5bd417feae9
ds-wook <leewook94@gmail.com>
2024-01-14 17:11:54 +0900

    Update!

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,10 @@
+import os
+import random
+
 import numpy as np
+import pandas as pd
+import torch
+from tqdm import tqdm
 
 
 def q1(x: np.ndarray) -> float:
@@ -7,3 +13,51 @@ def q1(x: np.ndarray) -> float:
 
 def q3(x: np.ndarray) -> float:
     return x.quantile(0.75)
+
+
+def seed_everything(seed: int = 42) -> None:
+    os.environ["PYTHONASHSEED"] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def reduce_mem_usage(df: pd.DataFrame, verbose: bool = True) -> pd.DataFrame:
+    """
+    Iterate through all the columns of a dataframe and modify the data type to reduce memory usage.
+    """
+    numerics = ["int16", "int32", "int64", "float16", "float32", "float64"]
+    start_mem = df.memory_usage().sum() / 1024**2
+
+    for col in tqdm(df.columns, leave=False):
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == "int":
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+
+    end_mem = df.memory_usage().sum() / 1024**2
+
+    if verbose:
+        print(f"Mem. usage decreased to {end_mem:5.2f} Mb ({100 * (start_mem - end_mem) / start_mem:.1f}% reduction)")
+
+    return df
